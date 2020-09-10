35cd76beaf7d99089bfa97f897695a011de5a2d9
Damilola Faseun <fashfired@gmail.com>
2020-09-10 15:17:26 +0100

    feat: Created the about component

---
+++ src/components/about/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+
+    return (
+      <div className="About">
+        <header className="about-header">
+          <h1>About Us</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default About;
