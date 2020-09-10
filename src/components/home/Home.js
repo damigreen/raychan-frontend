7d3e7fc1bc3b91194275e990c27cb810e2f291eb
Damilola Faseun <fashfired@gmail.com>
2020-09-10 15:18:41 +0100

    feat: Added Home component

---
+++ src/components/home/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Home.scss'
+import { Layout } from 'antd';
+
+const { Header } = Layout;
+
+
+class Home extends Component {
+  render() {
+
+    return (
+      <div id="wrap" className="home">
+            <h1>Raychan Home</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
