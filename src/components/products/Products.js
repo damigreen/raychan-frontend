bf76b7bf9b5afc032729ee67eeb0126f5f58fe61
Damilola Faseun <fashfired@gmail.com>
2020-09-10 15:19:04 +0100

    feat: Added Product component

---
+++ src/components/products/Products.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Products extends Component {
+  render() {
+
+    return (
+      <div id="wrap" className>
+            <h1>Raychan Products</h1>
+      </div>
+    );
+  }
+}
+
+export default Products;
