0ffb3e8aa4cd456c54a33e7c2b10ca9758a2dea5
Damilola Faseun <fashfired@gmail.com>
2020-09-12 15:52:17 +0100

    feat: Added treatment component

---
+++ src/components/treatments/Treatments.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import {
+  Row,
+  Col,
+  Typography
+} from 'antd'
+// import sickleCell from '../../images/sicklecell.png'
+import image from '../../images/Image.png'
+import './Treatment.scss'
+
+const { Title, Paragraph } = Typography
+
+class Treatments extends Component {
+  render() {
+
+    
+    return (
+      <div id="treatment">
+        <Title className="title-block">Ailments and Treatments</Title>
+        <Row id="treatment-row">
+            
+            <Col span={12} id="column-content">
+              <Title level={3}>Sickle Cell Anemia</Title>
+              <Paragraph className="column-paragraph">
+                Sickle Cell Anemia over the years has been perceived as one of the most threatening disorder to man-kind, some homes has been broken and couples separated, early death and delayed marriage due to this disorder.kkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+              </Paragraph>
+            </Col>
+
+            <Col span={12} className="image-block">
+              <img className="image-product" src={image} alt="Sickle cell image" />
+            </Col>
+          </Row>
+      </div>
+
+    );
+  }
+}
+
+export default Treatments;
