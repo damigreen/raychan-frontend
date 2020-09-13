3dbb3a750c4caad01b18ac3ddf01409a72e8e1ff
Damilola Faseun <fashfired@gmail.com>
2020-09-13 20:29:44 +0100

    Created the contact info component

---
+++ src/components/footer/contactInfo/ContactInfo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function ContactInfo() {
+
+
+  /* 
+  TODO
+  * Add address, email, phonenumber, 
+  * Include social media links
+
+  */
+  return(
+    <div>
+      <h2>Contact Info</h2>
+    </div>
+  )
+}
+
+export default ContactInfo
