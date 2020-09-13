6ac944b45a7ccfd31335b8199e3dd0cbf42d3fdf
Damilola Faseun <fashfired@gmail.com>
2020-09-13 20:28:55 +0100

    feat: Created the contact component

---
+++ src/components/footer/contact/Contact.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Form, Input, Button } from 'antd';
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+    email: '${label} is not validate email!',
+    number: '${label} is not a validate number!',
+  },
+  number: {
+    range: '${label} must be between ${min} and ${max}',
+  },
+};
+
+
+
+function Contact() {
+  const onFinish = (values) => {
+    console.log(values);
+  };
+  
+  return(
+    <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+      <Form.Item
+        name='name'
+        label="Name"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name='email'
+        label="Email"
+        rules={[
+          {
+            type: 'email',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item name='subject' label="Subject">
+        <Input />
+      </Form.Item>
+      <Form.Item name='message' label="Message">
+        <Input.TextArea />
+      </Form.Item>
+      <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+}
+
+export default Contact
