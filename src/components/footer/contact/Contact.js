49b9a67557dd71016e55e50c875ecd4cbe2b6751
Damilola Faseun <fashfired@gmail.com>
2020-09-13 21:13:07 +0100

    feat: added a contact us form

--- src/components/footer/contact/Contact.js
+++ src/components/footer/contact/Contact.js
@@ -28,43 +28,46 @@ function Contact() {
   };
   
   return(
-    <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
-      <Form.Item
-        name='name'
-        label="Name"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+    <div>
+      <h2>CONTACT US</h2>
+      <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+        <Form.Item
+          name='name'
+          label="Name"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        name='email'
-        label="Email"
-        rules={[
-          {
-            type: 'email',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          name='email'
+          label="Email"
+          rules={[
+            {
+              type: 'email',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item name='subject' label="Subject">
-        <Input />
-      </Form.Item>
-      <Form.Item name='message' label="Message">
-        <Input.TextArea />
-      </Form.Item>
-      <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item name='subject' label="Subject">
+          <Input />
+        </Form.Item>
+        <Form.Item name='message' label="Message">
+          <Input.TextArea />
+        </Form.Item>
+        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   )
 }
 
