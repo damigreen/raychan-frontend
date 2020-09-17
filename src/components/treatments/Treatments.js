99e12751d6e5128bddcb41726948b591d425325b
Damilola Faseun <fashfired@gmail.com>
2020-09-17 09:12:33 +0100

    modified the tratments cards

--- src/components/treatments/Treatments.js
+++ src/components/treatments/Treatments.js
@@ -36,7 +36,10 @@ function Treatments() {
 
           <Col span={8}>
             <Card className="card-style" title="Asthma" bordered={false}>
-              Card content
+            Asthma is a disease affecting the airways that carry air to and from your lungs. People who suffer from this chronic condition (long-lasting or recurrent) are said to be asthmatic.
+
+            Asthma can usually be managed with rescue inhalers to treat symptoms and controller inhalers that prevent symptoms
+
               <Button>
                 <Link to="/asthma">LEARN MORE</Link>
               </Button>
@@ -48,7 +51,7 @@ function Treatments() {
         <Row className="flex-column" gutter={30}>
           <Col span={8}>
             <Card className="card-style" title="Atrial Septal Defect" bordered={false}>
-              Perkinsons disease over the years has been perc
+            An atrial septal defect is a hole in the wall (septum) between the left and right atria. If there is a large hole, oxygen-rich blood from the left atrium flows back into the right atrium and gets pumped back to the lungs again. This causes more work for the heart
               <Button>
                 <Link to="/atrial-septal-defect">LEARN MORE</Link>
               </Button>
@@ -57,7 +60,7 @@ function Treatments() {
 
           <Col span={8}>
             <Card className="card-style" title="Parkinson's Disease" bordered={false}>
-              Card content
+            Parkinson’s disease is a progressive disorder of the nervous system that affects your movement. It develops gradually, sometimes starting with a barely noticeable tremor in just one hand. But while a tremor may be the most well-known sign of Parkinson’s disease, the disorder also commonly causes stiffness or slowing of movement.
               <Button>
                 <Link to="/parkinsons-disease">LEARN MORE</Link>
               </Button>
@@ -67,11 +70,6 @@ function Treatments() {
         </Row>
 
         <Row className="flex-column">
-          <Col>
-            <Button className="btn">
-              <Link to="/Ailments">AILMENTS AND REMEDIES</Link>
-            </Button>
-          </Col>
 
         </Row>
     </div>
