5d698f40d337c86f47faadfe73a1a2e4fd825243
Damilola Faseun <fashfired@gmail.com>
2020-09-24 10:57:01 +0100

    Added home component with hero image

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import heroImage from '../../images/hero/Hero.png'
+import './Home.scss'
 
 function Home() {
 
   return(
-    <h1>
-      Home
-    </h1>
+    <div id="hero">
+        <h1>
+          Get the best remedy and Ultimate cure from Sicknesses and diseases with Natural Herbal Medicines
+        </h1>
+        <h1>
+          Get the best remedy and Ultimate cure from Sicknesses and diseases with Natural Herbal Medicines
+        </h1>
+    </div>
   )
 }
 
