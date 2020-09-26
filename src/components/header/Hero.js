3de68726ed7d8d8c56d7c6668916380e4da54146
Damilola Faseun <fashfired@gmail.com>
2020-09-26 21:34:57 +0100

    feat: added a hero component

---
+++ src/components/header/Hero.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Hero.scss'
+import { Layout } from 'antd';
+
+const { Header } = Layout;
+
+
+class Hero extends Component {
+  render() {
+
+    return (
+      <div id="hero">
+        {/* <h1>
+          Get the best remedy and Ultimate cure from Sicknesses and diseases with Natural Herbal Medicines
+        </h1>
+        <h1>
+          Get the best remedy and Ultimate cure from Sicknesses and diseases with Natural Herbal Medicines
+        </h1> */}
+    </div>
+    );
+  }
+}
+
+export default Hero;
