5a4c5bd6bab57e6831131fc4abf153daa089451a
Damilola Faseun <fashfired@gmail.com>
2020-09-30 12:38:11 +0100

    Modified testimonial layout and style

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -6,16 +6,6 @@ import { LeftCircleOutlined, RightCircleOutlined } from '@ant-design/icons'
 
 const { Title } = Typography
 
-const contentStyle = {
-  // height: '160px',
-  // color: '#fff',
-  // lineHeight: '160px',
-  // margin: 0,
-  textAlign: 'center',
-  // width: '50%'
-  // background: '#364d79',
-};
-
 
 function Testimonials() {
   /* 
@@ -32,47 +22,14 @@ function Testimonials() {
 
   return(
     <div id="testimonials">
-      <Row>
-        <Col span={24}>
-          <Title className="header-small" level={4}>Testimonials</Title>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={24}>
-          <Title className="header-big" level={1}>What Our Customers are Saying</Title>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={12}>
-          <img className="quote-img" src={quote} alt="testimonial quote" />
-        </Col>
-      </Row>
+      <Title className="content-title small" level={4}>Testimonials</Title>
+      <Title className="content-title large" level={1}>What Our Customers Say</Title>
 
-      <Row className="carousel-row" span={24}>
-        <Col span={4}>
-          <LeftCircleOutlined className="left-arrow" onClick={prev} />
-        </Col>
-        <Col span={16}>
-          <Carousel ref={node =>carousel = node} className="carousel-block" arrows>
-          <div>
-            <p className="content-style">My baby's temperature has been very high over the past few weeks, we have given her all every drug the doctors prescribed. After using the medicine, my baby's temperature is stable now and she is smiling again.
-            </p>
-          </div>
-          <div>
-            <p className="content-style">My son experiences some pains within his body that usually keeps him down and can sometimes last days, but within some days after taking the medicine, he was completely relieved of his pain and became strong, started eating well and can do anything now.
-            </p>
-          </div>
-          <div>
-            <p className="content-style">I must confess, he has not had any crisis since he started taking the drug, he complained of slight pain this morning and he was feeling OK the next minute.
-            </p>
-          </div>
-          </Carousel>
-        </Col>
-        <Col className="right-arrow" span={4}>
-          <RightCircleOutlined className="right-arrow" onClick={next} />
-        </Col>
-      </Row>
-
+      <div className="testimonials-text">
+        <p className="content-style">My baby's temperature has been very high over the past few weeks, we have given her all every drug the doctors prescribed. After using the medicine, my baby's temperature is stable now and she is smiling again.</p><p className="content-style">My son experiences some pains within his body that usually keeps him down and can sometimes last days, but within some days after taking the medicine, he was completely relieved of his pain and became strong, started eating well and can do anything now.
+       </p><p className="content-style">I must confess, he has not had any crisis since he started taking the drug, he complained of slight pain this morning and he was feeling OK the next minute.</p>
+      </div>
+      
     </div>
   )
 }
