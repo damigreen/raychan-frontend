5246b7f4cd1ad17ae86ce43d547eaf46192d41a3
Damilola Faseun <fashfired@gmail.com>
2020-10-13 23:41:41 +0100

    Changed table to grid

--- src/components/cart/ShopCart.js
+++ src/components/cart/ShopCart.js
@@ -3,7 +3,7 @@ import './Cart.scss';
 import { Link } from 'react-router-dom';
 import xyz from '../../images/treatments/lowSpermCount/lowSpermCount.jpg';
 import './Cart.scss';
-import { Button } from 'antd';
+import { Row, Col, Button } from 'antd';
 import { CloseOutlined } from '@ant-design/icons';
 
 export default class ShopCart extends Component {
@@ -11,59 +11,102 @@ export default class ShopCart extends Component {
   * Add cancel botton on the image
   * Fix the images for the treatments
   * Sum all the products on the treatment and get the subtotal
+  * Change text colors
   * 
   */
   constructor(props) {
     super(props);
   }
 
-  render() { 
-    const { lowSpermCountShop = {}, shopItems = [] } = this.props;
-    const cartItemLen = shopItems.length;
+  componentDidMount() {
+  }
+  
+  componentDidUpdate() {
     
-    return (
+    this.props.getSubtotal();
+  }
+
+  render() { 
+    const { lowSpermCountShop = {}, shopItems = [], handleDelete } = this.props;
+    const cartItemLen = shopItems.length;
+
+    const subtotal = () =>
+      shopItems.reduce((acc, cur) => {
+      const value = acc + (cur.quantity * cur.price);
+      console.log(value)
+      // const valueString = value.toString();
+
+      var formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
       
+        // These options are needed to round to whole numbers if that's what you want.
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+      });
+
+      return formatter.format(value);
+    }, 0);
+
+
+    console.log(subtotal());
+    return (
       <div id="shop-cart">
-        <table id="cart-table" className="shop-cart-table">
-          <thead className="shop-cart-head">
-            <th>{cartItemLen} ITEMS</th>
-            <th>
-              <Link to="/Cart">VIEW CART</Link>
-            </th>
-          </thead>
-          <tbody className="shop-cart-body">
-            {
-              shopItems.map(item => (
-                <tr className="shop-cart-row" key={item.id}>
-                  <td className="item-image">
-                    <a>
-                      <img src={require('../../images/treatments/thumbnail/' + item.image)} />
-                    </a>
-                  </td>
-                  <td>
+
+    
+        <Row className="shop-cart-head">
+            <Col span={12}>
+              <div >
+                {cartItemLen} ITEMS
+              </div>
+            </Col>
+
+            <Col span={12}>
+              <div>
+                VIEW CART
+              </div>
+            </Col>
+        </Row>
+
+        
+        {
+          shopItems.map(item => (
+            <div>
+              <Row className="shop-cart-body">
+                  <Col span={7}>
+                      <a>
+                        <img src={require('../../images/treatments/thumbnail/' + item.image)} />
+                      </a>
+                  </Col>
+
+                  <Col span={13}>
                     {item.description}<br />
-                    {item.currency}{item.price}
-                  </td>
-                  <td>
-                    <a className="remove-from-cart">
-                      {/* <CloseOutlined /> */}
+                    {item.quantity} x {item.currency}{item.price}
+                  </Col>
+
+                  <Col span={4}>
+                    <Button onClick={() => handleDelete(item.id)} className="remove-from-cart">
                       x
-                    </a>
-                  </td>
-                </tr>
-              ))
-            }
-            <tr>
-              <td>
-                Subtotal
-              </td>
-              <td>
-                #400 000
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <Button>CHECKOUT</Button>
+                    </Button>
+                  </Col>
+              </Row>
+
+
+            </div>
+
+          ))
+        }
+
+        <Row>
+          <Col span={20}>
+            SUBTOTAL:
+          </Col>
+
+          <Col span={4}>
+            ₦{subtotal()}
+          </Col>
+        </Row>
+        <Button className="btn btn-checkout">CHECKOUT</Button>
       </div>
     )
   }
