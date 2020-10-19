f923319c23048d2cd3d9dfbbe66755355835aaeb
Damilola Faseun <fashfired@gmail.com>
2020-10-19 08:34:35 +0100

    removed the strict mode for react

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import 'antd/dist/antd.css';
 import 'typeface-open-sans';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
