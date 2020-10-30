5816571ea9d6ff802c34021f24defb2f7c537523
Damilola Faseun <fashfired@gmail.com>
2020-10-30 09:51:27 +0100

    Enclosed hero image in header

--- src/components/header/Hero.js
+++ src/components/header/Hero.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import './Hero.scss'
+import  heroImage from '../../images/hero/Hero.png';
 
 class Hero extends Component {
   render() {
 
     return (
       <div id="header">
-        <div id="hero">
-          {/* <h1>
+        {/* <div id="hero">
+          <h1>
             Get the best remedy and Ultimate cure from Sicknesses and diseases with Natural Herbal Medicines
           </h1>
           <h1>
             Get the best remedy and Ultimate cure from Sicknesses and diseases with Natural Herbal Medicines
-          </h1> */}
+          </h1>
+        </div> */}
+        <div className="hero">
+          <img src={heroImage} />
         </div>
       </div>
     );
