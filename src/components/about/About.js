abd4723da6030b7ba27561109f9e6877a1a94b5f
Damilola Faseun <fashfired@gmail.com>
2020-11-01 14:19:45 +0100

    Modified the font size of about heading

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -21,10 +21,10 @@ class About extends Component {
         <Col span={24} className="content-column">
           <article className="content-article">
             <header >
-              <h1>We Provide You With Natural Herbal Cure for a Healthy Living</h1>
+              <h1 className="content-header">We Provide You With Natural Herbal Cure for a Healthy Living</h1>
             </header>
 
-            <p>The uses of medicinal herbs is to prevent and treat diseases, ailment and to promote health and healing
+            <p className="content-body">The uses of medicinal herbs is to prevent and treat diseases, ailment and to promote health and healing
             If you are looking for Herbal Medicine or supplement that are actually effective for sickle cell and that would safe you the stress from taken pills everyday, going through series of crisis and complications <span style={{color: '#228B22'}}>Rayckan Herbal Health Care medicine is the solution</span>
             </p>
           </article>
