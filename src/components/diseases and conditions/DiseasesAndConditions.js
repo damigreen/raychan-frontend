e6ea793afd0a5d6b631d8c9f06bafe9fdfa91f06
Damilola Faseun <fashfired@gmail.com>
2020-12-30 08:59:33 +0100

    Added diseases and conditions

--- src/components/diseases and conditions/DiseasesAndConditions.js
+++ src/components/diseases and conditions/DiseasesAndConditions.js
@@ -4,6 +4,7 @@ import Asthma from './asthma/';
 import AtrialSeptalDefect from './atrial septal defect/';
 import LowSpermCount from './low sperm count/';
 import Parkinsons from './parkinsons/';
+import Ulcer from './ulcer/';
 import './Diseases.scss';
 
 import {
@@ -18,7 +19,8 @@ function DiseasesAndConditions({products, addToCart, shopItems, handleDelete, su
     { name: 'atrial septal defect', label: 'atrial-septal-defect', component: AtrialSeptalDefect },
     { name: 'low sperm count', label: 'low-sperm-count', component: LowSpermCount },
     { name: 'parkinsons disease', label: 'parkinsons-disease', component: Parkinsons },
-  ]
+    { name: 'ulcer', label: 'ulcer', component: Ulcer },
+  ];
 
   const diseasesMenu = diseases.map(diseasesInfo => {
     const { label, component: DiseasesComponent } = diseasesInfo
