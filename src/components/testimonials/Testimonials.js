f808a1f6efe93c9d2f9687c2b1860f83be892df0
Damilola Faseun <damilola.faseun@gmail.com~>
2021-06-26 12:40:18 +0100

    testimonial quotes

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -1,8 +1,6 @@
 import React, { useRef } from 'react'
 import './Testimonials.scss'
-import quote from '../../images/icons/quote-left-50.png'
-import { Row, Col, Typography, Carousel } from 'antd'
-import { LeftCircleOutlined, RightCircleOutlined } from '@ant-design/icons'
+import { Typography} from 'antd'
 
 const { Title } = Typography
 
@@ -30,8 +28,9 @@ function Testimonials() {
         </div>
 
         <div className="testimonials-text">
-          <p className="content-style">My baby's temperature has been very high over the past few weeks, we have given her all every drug the doctors prescribed. After using the medicine, my baby's temperature is stable now and she is smiling again.</p><p className="content-style">My son experiences some pains within his body that usually keeps him down and can sometimes last days, but within some days after taking the medicine, he was completely relieved of his pain and became strong, started eating well and can do anything now.
-         </p><p className="content-style">I must confess, he has not had any crisis since he started taking the drug, he complained of slight pain this morning and he was feeling OK the next minute.</p>
+          <p className="content-style">My baby's temperature has been very high over the past few weeks, we have given her all every drug the doctors prescribed. After using the medicine, my baby's temperature is stable now and she is smiling again. <span style={{display: 'block', marginTop: '1rem', fontSize: '0.9899rem', marginLeft: '1rem', color: '#391a45', fontWeight: '400', fontStyle: 'italic'}}>Miss. Mary S. </span></p>
+          <p className="content-style">My son experiences some pains within his body that usually keeps him down and can sometimes last days, but within some days after taking the medicine, he was completely relieved of his pain and became strong, started eating well and can do anything now.<span style={{display: 'block', fontSize: '0.9899rem', marginLeft: '1rem', color: '#391a45', fontWeight: '400', fontStyle: 'italic'}}>Sarah</span></p>
+          <p className="content-style">I must confess, he has not had any crisis since he started taking the drug, he complained of slight pain this morning and he was feeling OK the next minute.<span style={{display: 'block', marginTop: '1rem', fontSize: '0.9899rem', marginLeft: '1rem', color: '#391a45', fontWeight: '400', fontStyle: 'italic'}}>Mr. Ayomide Samuel</span></p>
         </div>
 
       </div>
